042109da9430f3244a8ea014ab65b991173f540d
GEORGEMUTALE <mutalegeorge367@gmail.com>
2024-04-14 01:04:39 +0200

    Bob is funny

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,29 @@
 pub fn reply(message: &str) -> &str {
-    unimplemented!("have Bob reply to the incoming message: {message}")
+    let trimmed_message = message.trim();
+
+    if is_silence(trimmed_message) {
+        "Fine. Be that way!"
+    } else if is_question(trimmed_message) {
+        if is_yelling(trimmed_message) {
+            "Calm down, I know what I'm doing!"
+        } else {
+            "Sure."
+        }
+    } else if is_yelling(trimmed_message) {
+        "Whoa, chill out!"
+    } else {
+        "Whatever."
+    }
+}
+
+fn is_silence(message: &str) -> bool {
+    message.is_empty()
+}
+
+fn is_question(message: &str) -> bool {
+    message.ends_with('?')
+}
+
+fn is_yelling(message: &str) -> bool {
+    message == message.to_uppercase() && message.chars().any(|c| c.is_alphabetic())
 }
